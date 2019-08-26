38aba2db61baa0e87f3f7aeea77496fb6c4d6745
Memorisza <aut14030@hotmail.com>
2019-08-26 15:38:30 +0700

    Init Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      EIEI
+    </div>
+  );
+}
+
+export default App;
