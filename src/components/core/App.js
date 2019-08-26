732771f7bdae1a18a5c0f4c1aa362e90a38d657e
Memorisza <aut14030@hotmail.com>
2019-08-26 16:10:54 +0700

    Added Header & Navbar (Tested)

---
+++ src/components/core/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Header from 'Header';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/core/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Navbar from 'Navbar';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <Navbar/>
+        )
+    }
+}
